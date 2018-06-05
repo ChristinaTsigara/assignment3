02fbb16d859962d6cc6c36b58050de59c4e82536
ChristinaTsigara <christina.tsigara@gmail.com>
2018-06-05 14:33:01 +0200

    Added authentication

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::group(["middleware" => ["auth"]], function() {
+    Route::resource('programs', 'ProgramController', ['only' => ['create', 'store', 'edit', 'update', 'destroy']]);
+    Route::resource('students', 'StudentController', ['only' => ['create', 'store', 'edit', 'update', 'destroy']]);
+    Route::resource('courses', 'CourseController', ['only' => ['create', 'store', 'edit', 'update', 'destroy']]);
+
+});
+
+Route::resource('programs', 'ProgramController', ['only' => ['index', 'show']]);
+Route::resource('students', 'StudentController', ['only' => ['index', 'show']]);
+Route::resource('courses', 'CourseController', ['only' => ['index', 'show']]);
